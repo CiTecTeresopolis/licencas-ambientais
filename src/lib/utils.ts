a2051276f2912779647c6f8a1588c2727780832a
neves369 <douglasbrian369@hotmail.com>
2026-03-30 16:45:03 -0300

    nova função

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,12 +7,12 @@ export function cn(...inputs: ClassValue[]) {
 
 export const CHART_COLORS = [
   "#264653", "#2a9d8f", "#dfa208", "#f4a261", "#e76f51",
-  "#8bc34a", "#00bcd4", "#3f51b5", "#673ab7", "#e91e63",
-  "#8ab17d", "#b56576", "#588157", "#3d5a80", "#98c1d9",
-  "#e0fbfc", "#ee6c4d", "#293241",
+  "#8bc34a", "#00bcd4", "#3f51b5", "#b9154cd0", "#98c1d9",
+  "#8ab17d", "#b56576", "#588157", "#3d5a80", "#293241",
+  "#e0fbfc", "#ee6c4d", "#673ab7",
 ];
 
-export function getModalidadeColor(name: string, allModalidades: {name: string}[]) {
+export function getModalidadeColor(name: string, allModalidades: { name: string }[]) {
   const index = allModalidades.findIndex((m) => m.name === name);
   if (index === -1) return "#cccccc";
   if (index < CHART_COLORS.length) return CHART_COLORS[index];
